969627006017ce87bee441f257127176ae365119
Mingsicogi <codedoctor119@gmail.com>
2019-10-23 03:08:47 +0900

    + config update

--- src/main/java/com/example/demo/test/Welcome.java
+++ src/main/java/com/example/demo/test/Welcome.java
@@ -2,14 +2,21 @@ package com.example.demo.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Mono;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -20,21 +27,53 @@ public class Welcome {
     @Autowired
     private MyService myService;
 
-    String url1 = "http://localhost:18083/service1?req={req}";
-    String url2 = "http://localhost:18083/service2?req={req}";
+    private String url1 = "http://localhost:18083/service1?req={req}";
+    private String url2 = "http://localhost:18083/service2?req={req}";
 
-    WebClient client = WebClient.create();
+    private WebClient client = WebClient.create();
+
+    @GetMapping("/ip")
+    public ResponseEntity<String> getMyIp(){
+        String systemipaddress = "";
+
+        try {
+
+            URL url_name = new URL("http://bot.whatismyipaddress.com");
+            BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()));
+            systemipaddress = sc.readLine().trim();
+            if (!(systemipaddress.length() > 0))
+            {
+                try
+                {
+                    InetAddress localhost = InetAddress.getLocalHost();
+                    log.info((localhost.getHostAddress()).trim());
+                    systemipaddress = (localhost.getHostAddress()).trim();
+                }
+                catch(Exception e1)
+                {
+                    systemipaddress = "Cannot Execute Properly";
+                }
+            }
+        }
+        catch (Exception e2) {
+            systemipaddress = "Cannot Execute Properly";
+        }
+
+        log.info("\nYour IP Address: " + systemipaddress +"\n");
+
+        return new ResponseEntity<>(systemipaddress, HttpStatus.OK);
+    }
 
     @GetMapping("/hello/webFlux")
     public Mono<String> welcome(Integer idx){
         return client.get().uri(url1, idx).exchange()
                 .flatMap(clientResponse -> clientResponse.bodyToMono(String.class))
-                .doOnNext(c -> log.info("{}", c))
+                .doOnNext(c -> log.info("1 : {}", c))
                 .flatMap(response -> client.get().uri(url2, response).exchange())
                 .flatMap(clientResponse -> clientResponse.bodyToMono(String.class))
-                .doOnNext(c -> log.info("{}", c))
+                .doOnNext(c -> log.info("2 : {}", c))
                 .flatMap(res -> Mono.fromCompletionStage(myService.work(res)))
-                .doOnNext(c -> log.info("{}", c));
+                .doOnNext(c -> log.info("3 : {}", c));
     }
 
     @Service("myService")
