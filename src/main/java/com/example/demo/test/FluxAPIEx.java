cf65050b6cd3ca5398957cdb796b9c4323dbc6a2
Mingsicogi <codedoctor119@gmail.com>
2019-10-30 01:51:23 +0900

    + mono, flux api demo

--- src/main/java/com/example/demo/test/FluxAPIEx.java
+++ src/main/java/com/example/demo/test/FluxAPIEx.java
@@ -1,12 +1,20 @@
 package com.example.demo.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/flux/ex")
+@Slf4j
 public class FluxAPIEx {
 
     /**
@@ -15,9 +23,94 @@ public class FluxAPIEx {
      * @return
      */
     @GetMapping("/1")
-    public Flux<String> ex1(){
-        Flux<String> stringFlux = Flux.just("Hello Flux ~ ", " HI Flux ~~");
+    public Flux<Integer> ex1(){
+        Flux<Integer> stringFlux = Flux.just(1,2,3,4,5,6,7,8,9,10);
 
         return stringFlux;
+    }
+
+    /**
+     * 데이터를 내보내는 flux 객체 생성
+     *
+     * @return
+     */
+    @GetMapping("/2")
+    public Flux<String> ex2(){
+        Flux.just(1,2,3,4,5,6,7,8,9,10).log().subscribe(new Subscriber<>() {
+            private Subscription s = null;
+            private int count;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                s = subscription;
+                subscription.request(2);
+            }
+
+            /**
+             * back pressure - subscriber의 요청에 맞게 데이터를 받
+             *
+             * @param integer
+             */
+            @Override
+            public void onNext(Integer integer) {
+                log.info("### {}", integer);
+                if(count++ % 2 == 0){
+                    s.request(2);
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        return Flux.just("OK");
+    }
+
+    /**
+     * Map operation
+     *
+     * @return
+     */
+    @GetMapping("/3")
+    public Flux<Integer> ex3(){
+        Flux<Integer> stringFlux = Flux.just(1,2,3,4,5,6,7,8,9,10);
+
+        return stringFlux.log().map(num -> num * 2);
+    }
+
+    /**
+     * Combine two stream
+     *
+     * @return
+     */
+    @GetMapping("/4")
+    public Flux<Integer> ex4(){
+        Flux<Integer> stringFlux = Flux.just(1,2,3,4,5,6,7,8,9,10);
+
+        return stringFlux.log().map(num -> num * 2);
+    }
+
+    /**
+     * Combine two stream
+     *
+     * @return
+     */
+    @GetMapping("/5")
+    public Flux<Object> ex5(){
+
+        ConnectableFlux<Object> stringFlux = Flux.create(fluxSink -> {
+           while(true){
+               fluxSink.next(System.currentTimeMillis());
+           }
+        }).publish();
+
+        return stringFlux.autoConnect().log().take(30);
     }
 }
--- src/main/java/com/example/demo/test/MonoApiEx.java
+++ src/main/java/com/example/demo/test/MonoApiEx.java
@@ -1,16 +1,24 @@
 package com.example.demo.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 @RestController
 @RequestMapping("/mono/ex")
+@Slf4j
 public class MonoApiEx {
 
     /**
-     * 데이터를 내보내는 mono 객체 생성
+     * 데이터를 내보내는 mono 객체 생성 1
      *
      * @return
      */
@@ -18,6 +26,31 @@ public class MonoApiEx {
     public Mono<String> ex1(){
         Mono<String> stringMono = Mono.just("Hello Mono ~ ");
 
-        return stringMono;
+        return stringMono.log();
+    }
+
+    /**
+     * 데이터를 내보내는 mono 객체 생성 2
+     *
+     * @return
+     */
+    @GetMapping("/2")
+    public Mono<String> ex2(){
+        List<String> strList = Arrays.asList("a","ab","abc","bc");
+        Mono<String> emptyMono = Mono.justOrEmpty(strList.stream().filter(str -> str.startsWith("d")).findFirst());
+
+        return emptyMono.log();
+    }
+
+    /**
+     * Map operation
+     *
+     * @return
+     */
+    @GetMapping("/3")
+    public Mono<List<Integer>> ex3(){
+        List<Integer> strList = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).map(i -> i * 2).collect(toList());
+
+        return Mono.just(strList).log();
     }
 }
